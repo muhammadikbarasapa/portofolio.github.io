2589fc417e627026aff16b0133ab213360e16bde
muhammadikbarasapa <muhammadikbarasapa@smkwikrama.sch.id>
2023-11-16 12:35:41 +0700

    wajib ngulik

--- ngulik.js
+++ ngulik.js
@@ -7,7 +7,7 @@ window =addEventListener("scroll",function() {
 let menu = document.querySelector('#menu--icon');
 let navbar = document.querySelector('navbar');
 
-menu.onclick = () => {
+menu.onclick =()=> {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
