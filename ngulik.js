523522a0ae02d4eaf7897e784cd4b36f649718b6
muhammadikbarasapa <muhammadikbarasapa@smkwikrama.sch.id>
2023-11-17 10:19:55 +0700

    ngulik.js

--- ngulik.js
+++ ngulik.js
@@ -4,10 +4,10 @@ window =addEventListener("scroll",function() {
     header.classList.toggle("sticky",window.scrollY > 60)
 });
 
-let menu = document.querySelector('#menu--icon');
+let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('navbar');
 
-menu.onclick = function () {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
